a1b79ff150893c6e7a1157f3305df28c291a9241
bagel <156224735+ekanshgouttam@users.noreply.github.com>
2026-09-08 23:06:19 +0530

    Time: 15 ms (63.89%), Space: 32.2 MB (79.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n = len(nums)
-        nums.sort()
-        max_count = 0
-        for i in range(n-1):
-            count = 0
-            if nums[i] == nums[i+1]:
-                count += 1
-            max_count = max(max_count, count)
-        if max_count >0:
-            return True
-        else:
-            return False
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
 
+            hash_set.add(num)
+        return False
