b4a7381c334a781473943287a2b209f864f8626b
bagel <156224735+ekanshgouttam@users.noreply.github.com>
2026-09-08 23:27:37 +0530

    Time: 11 ms (77.28%), Space: 19.4 MB (46.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        hash_s, hash_t =  {}, {}
+        for i in range(len(s)):
+            hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
+            hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
+        for c in hash_s:
+            if hash_s[c] != hash_t.get(c, 0):
+                return False
+        return True
